ed8cb111e0555d4621ea5f861e87b05e46937097
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-10-13 21:46:02 +0000

    Creating branch for BioC 3.0 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_0/madman/Rpacks/GSEABase@95441 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/utilities.R
@@ -0,0 +1,36 @@
+## Placeholder 'till something appropriate decided
+.uniqueIdentifier <- function()
+{
+    paste(Sys.info()['nodename'], Sys.getpid(), Sys.time(),
+          sample(.Machine$integer.max, 1), sep=":")
+}
+
+## simplified unique for vectors, preserving attributes
+.unique <- function(x, y) c(x, y[!(y %in% x)])
+
+.glue <- function(x, y, op)
+    paste("(", x, op, y, ")", sep="")
+
+.requireQ <- function(pkg)
+  suppressWarnings(require(pkg, quietly=TRUE, character.only=TRUE))
+
+.nameAll <- function(x) {
+    ## Add names to character vector x.  Elements of x without names get
+    ## a name matching the element.
+    ##
+    if (!is.character(x))
+      stop("argument 'x' must be a character vector")
+    if (length(names(x)))
+      names(x) <- ifelse(nchar(names(x)) == 0, x, names(x))
+    else
+      names(x) <- x
+    x
+}
+
+.stopf <- function(...)
+    stop(simpleError(sprintf(...),
+                     call=match.call(call=sys.call(sys.parent(1)))))
+
+.warningf <- function(...)
+    warning(simpleWarning(sprintf(...),
+                          call=match.call(call=sys.call(sys.parent(1)))))
